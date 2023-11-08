137dd546760cd771a43073a15735af6ccd4331c5
ajsolutionpvtltd <numanaljilani001@gmail.com>
2023-11-08 18:55:44 +0530

    update

--- lib/models/community.model.ts
+++ lib/models/community.model.ts
@@ -1,9 +1,23 @@
 import mongoose from "mongoose";
 
 const communitySchema = new mongoose.Schema({
-   
-   
-   
+    id : { type : String , required : true },
+    name : { type : String , required : true },
+    username : { type : String , required : true , unique : true },
+    image : String , 
+    bio : String,
+    threads : [
+        {
+            type : mongoose.Schema.Types.ObjectId,
+            ref : "Thread"
+        }
+    ],
+    members : [
+        {
+            type : mongoose.Schema.Types.ObjectId,
+            ref : "User"
+        }
+    ]
 })
 
  const  Community : any = mongoose.models.Community || mongoose.model("Community",communitySchema)
--- next.config.js
+++ next.config.js
@@ -1,5 +1,8 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
+  typescript:{
+    ignoreBuildErrors: false, 
+  },
     experimental: {
       serverActions: true,
       serverComponentsExternalPackages: ["mongoose"],
